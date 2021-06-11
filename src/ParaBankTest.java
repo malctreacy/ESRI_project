c1eec0536e233fa57ba4691c998feb0deba612d6
malctreacy <mtreacy@uci.edu>
2021-06-10 20:31:06 -0700

    Added bank site url, initial element targets

--- src/ParaBankTest.java
+++ src/ParaBankTest.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
 
 public class ParaBankTest {
     public static void main(String [] args) throws InterruptedException {
@@ -6,8 +8,12 @@ public class ParaBankTest {
         FirefoxDriver driver = new FirefoxDriver();
 
         // Parabank Mainpage
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
 
         // Target Login Elements
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div/form/div[3]/input"));
 
         // Close out Driver
         driver.quit();
